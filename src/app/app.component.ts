247d7fdf601b3209f2e3f39d5054c9b2766f835b
anshbegmirza <anshbegmirza@gmail.com>
2025-06-17 19:03:30 +0530

    drag and drop function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import {
+  Component,
+  OnDestroy,
+  OnInit,
+  ElementRef,
+  ViewChild,
+} from '@angular/core';
 import { Tile } from './shared/tile.model';
 import { DragService } from './shared/drag.service';
 import { Subscription, timer } from 'rxjs';
@@ -11,10 +17,15 @@ import { CdkDragEnd } from '@angular/cdk/drag-drop';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit, OnDestroy {
+  @ViewChild('containerScreen', { static: false })
+  containerScreen!: ElementRef;
+
   droppedItems: any[] = [];
   liveTime: Date | Number | string = new Date();
 
   showDeleteDialog: boolean = false;
+
+  draggedTile: any = null;
 
   optionsSelect: any[] = [
     { value: '1', viewValue: '1' },
@@ -22,71 +33,122 @@ export class AppComponent implements OnInit, OnDestroy {
     { value: '3', viewValue: '3' },
   ];
 
-  idCounter: number = 0;
+  idCounter: number = 1;
 
-  private timeSubscription: Subscription;
-
-  tileOptions: any[] = [
-    { type: 'text', label: 'Text Input' },
-    { type: 'image', imageSrc: '../assets/4320405.png' },
-    { type: 'dropdown', label: 'Select Options', options: this.optionsSelect },
-    { type: 'timer', label: this.liveTime },
-  ];
+  tileOptions: any[] = [];
 
   constructor(private dragService: DragService) {
-    this.timeSubscription = timer(0, 1000)
-      .pipe(map(() => new Date().toLocaleTimeString()))
-      .subscribe((now) => {
-        this.liveTime = now;
-      });
+    console.log(this.containerScreen);
   }
 
   ngOnInit() {
-    this.timeSubscription = timer(0, 1000)
-      .pipe(map(() => new Date()))
-      .subscribe((now) => {
-        this.liveTime = now.toLocaleString('en-US', {
-          hour: '2-digit',
-          minute: '2-digit',
-          second: '2-digit',
-          hour12: false,
-        });
-      });
+    this.startClock();
     this.loadFromLocalStorage();
+    this.initializeTileOptions();
   }
 
-  ngOnDestroy() {
-    if (this.timeSubscription) {
-      this.timeSubscription.unsubscribe();
-    }
+  startClock() {
+    setInterval(() => {
+      const now = new Date();
+      this.liveTime = this.liveTime = now.toLocaleString('en-US', {
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+        hour12: false,
+      });
+    }, 1000);
   }
+
+  initializeTileOptions() {
+    this.tileOptions = [
+      { type: 'text', label: 'Text Input' },
+      { type: 'image', imageSrc: '../assets/4320405.png' },
+      {
+        type: 'dropdown',
+        label: 'Select Options',
+        options: this.optionsSelect,
+      },
+      { type: 'timer', label: this.liveTime },
+    ];
+  }
+
+  ngOnDestroy() {}
 
   generateID(): number {
     return this.idCounter++;
   }
 
-  onDragStart(event: DragEvent, tile: any) {
-    const clonedTile = {
-      ...tile,
-      id: this.generateID(),
-      x: tile.x,
-      y: tile.y,
-    };
-    this.droppedItems.push(clonedTile);
+  onDragStart(tile: any) {
+    console.log(tile);
+
+    this.draggedTile = tile;
   }
 
+  moveTile(event: any) {
+    // it does some work.
+    console.log(event);
+
+    const screenRect =
+      this.containerScreen.nativeElement.getBoundingClientRect();
+    console.log(screenRect);
+    const x = event.clientX - screenRect.left;
+    const y = event.clientY - screenRect.top;
+    console.log(x, y);
+
+    this.droppedItems.filter((id) => {
+      console.log(id);
+
+      if (id.id === this.draggedTile.id) {
+        this.draggedTile = {
+          ...this.draggedTile,
+          x: x,
+          y: y,
+        };
+      }
+    });
+    console.log(this.droppedItems);
+
+    // this.draggedTile = {
+    //   ...this.draggedTile,
+    //   x: x,
+    //   y: y,
+    // };
+    this.droppedItems.push(this.draggedTile);
+    this.saveToLocalStorage();
+  }
+
+  // for left side tiles
   onDragEnded(event: any, tile?: any): void {
     console.log(tile);
-    const { x, y } = event.source.getFreeDragPosition();
+    console.log(event.clientX, event.clientY);
+
+    if (
+      tile.type === 'image' &&
+      this.droppedItems.some((t) => t.type === 'image')
+    )
+      return;
+
+    if (
+      tile.type === 'timer' &&
+      this.droppedItems.some((t) => t.type === 'timer')
+    )
+      return;
+
+    const screenRect =
+      this.containerScreen.nativeElement.getBoundingClientRect();
+    console.log(screenRect);
+    const x = event.clientX - screenRect.left;
+    const y = event.clientY - screenRect.top;
     console.log(x, y);
 
     const clonedTile = {
       ...tile,
       id: this.generateID(),
-
       x: x,
       y: y,
     };
+    console.log(clonedTile);
+
     this.droppedItems.push(clonedTile);
     console.log(this.droppedItems);
     this.saveToLocalStorage();
@@ -97,8 +159,10 @@ export class AppComponent implements OnInit, OnDestroy {
   }
 
   onReset() {
-    localStorage.removeItem('droppedItems');
+    this.droppedItems = [];
+    this.saveToLocalStorage();
     this.showDeleteDialog = !this.showDeleteDialog;
+    console.log('dropped items are', this.droppedItems);
   }
 
   saveToLocalStorage() {
@@ -106,6 +170,12 @@ export class AppComponent implements OnInit, OnDestroy {
   }
 
   loadFromLocalStorage() {
-    // this.droppedItems = localStorage.getItem('droppedItems');
+    let saved = localStorage.getItem('droppedItems');
+    console.log(saved);
+
+    if (saved) {
+      this.droppedItems = JSON.parse(saved);
+      console.log('loaded from local storage', this.droppedItems);
+    }
   }
 }
